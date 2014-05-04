bd7c5c998edd5918d36223ba7791c2fd0be5cbfa
bo <bo@bo-VirtualBox.(none)>
2014-05-03 21:37:09 -0400

    step2 admin page
    Signed-off-by: bo <bo@bo-VirtualBox.(none)>

--- polls/admin.py
+++ polls/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import Choice, Poll
+
+class ChoiceInline(admin.StackedInline):
+	model = Choice
+	extra = 3
 
 class PollAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None, {'fields': ['question']}),
 		('Date information', {'fields': ['pub_date']})
 	]
+	inlines = [ChoiceInline]
+
+	list_display = ('question','pub_date', 'was_published_recently', '__str__')
+	list_filter = ['pub_date', 'question']
+	search_fields = ['question']
+	
 
 admin.site.register(Poll, PollAdmin)
